3aa7c03b40690047f02efbb76fc1d5220d70a885
Ryo Kawaguchi <max_squis0776@icloud.com>
2026-06-27 07:28:20 +0900

    first commit

---
+++ src/test/java/com/example/globelink/GlobelinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.globelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobelinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

8c4554f1cb084c53d97a0dcb9ffc5ee5ed10084a
Ryo Kawaguchi <maximum.river1211@gmail.com>
2026-06-27 07:28:20 +0900

    first commit

---
+++ src/main/java/com/example/globelink/GlobelinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.globelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobelinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobelinkApplication.class, args);
+	}
+
+}

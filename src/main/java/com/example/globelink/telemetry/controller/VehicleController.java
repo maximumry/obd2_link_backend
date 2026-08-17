153437b758b34b6e4be1d64cc32c318091203395
Ryo Kawaguchi <max_squis0776@icloud.com>
2026-08-17 23:06:31 +0900

    テレメトリー取得n

--- src/main/java/com/example/globelink/telemetry/controller/VehicleController.java
+++ src/main/java/com/example/globelink/telemetry/controller/VehicleController.java
@@ -1,6 +1,18 @@
 package com.example.globelink.telemetry.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
 public class VehicleController {
+
+    @GetMapping("telemetry")
+    public String getTelemetry(@RequestParam String param) {
+        return new String();
+    }
+    
     
 }
 

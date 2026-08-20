4d2e4ea37ea4f623e105bcd166af2de9d17eea27
Ryo Kawaguchi <max_squis0776@icloud.com>
2026-08-20 22:53:03 +0900

    車両データ取得とポストの

--- src/main/java/com/example/globelink/telemetry/controller/VehicleController.java
+++ src/main/java/com/example/globelink/telemetry/controller/VehicleController.java
@@ -2,17 +2,22 @@ package com.example.globelink.telemetry.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-@RestController
+@RestController("/api/v1/telemetry/")
 public class VehicleController {
 
-    @GetMapping("telemetry")
-    public String getTelemetry(@RequestParam String param) {
+    @GetMapping("{deviceId}")
+    public String getTelemetry(@PathVariable String deviceId, @RequestParam String param) {
         return new String();
     }
+
     
-    
+    @PostMapping("{deviceId}/start")
+    public String postTelemetry(@PathVariable String deviceId, @RequestParam String param){
+        
+    }    
 }
 

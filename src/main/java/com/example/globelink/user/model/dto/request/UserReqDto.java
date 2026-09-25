ccd56f5324d0ed4458037c2fb29f0bb05450bff2
Ryo Kawaguchi <max_squis0776@icloud.com>
2026-09-26 03:19:07 +0900

    バリデーションを追加しました。

--- src/main/java/com/example/globelink/user/model/dto/request/UserReqDto.java
+++ src/main/java/com/example/globelink/user/model/dto/request/UserReqDto.java
@@ -1,5 +1,8 @@
 package com.example.globelink.user.model.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data 
@@ -8,11 +11,16 @@ public class UserReqDto {
     /**
      * メールアドレス
      */
+    @NotBlank(message = "メールアドレスは必須です")
+    @Size(max = 254, message = "メールアドレスは254文字以内で入力して下さい")
+    @Email(message = "メールアドレスの形式が正しくありません")
     private String email;
 
     /**
      * 表示名
      */
+    @NotBlank(message = "ユーザー名は必須です")
+    @Size(max = 64, message = "ユーザー名は64文字以内で入力して下さい")
     private String displayName;
 
 }
